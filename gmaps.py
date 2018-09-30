24fa074a5bba40f6973a64dca86456ff545d4f1d
L <41794465+LLL15@users.noreply.github.com>
2018-09-30 16:18:08 +0200

    Update gmaps.py
    - Added comments explaining the code
    - Added while loop which will ask the user if he wants to continue or not after every searched location
    - Improved code readability

--- gmaps.py
+++ gmaps.py
@@ -1,19 +1,46 @@
 #! /usr/bin/python3
+# Imports needed modules
 import sys, webbrowser
 
-try:
+try: # try/except for importing pyperclip module
     import pyperclip
-except ImportError:
+except ImportError: # If pyperclip was not found, error is displayed to user and program exits
     print('!Error - pyperclip module not found, check README for help!\nExiting...')
     sys.exit()
 
+
+# openGMaps function is where it all happens
+def openGMaps(args):
+    while True:
+
+        if len(args) > 1: # If len of args is greater than 1
+            # It creates address variable where it stored all joined arguments (user's input)
+            address = ''.join(args[:])
+
+        else:
+            # Then it assumes that the user's wishful location is in his clipboard
+            # so it stores the first thing on his clipboard into address variable.
+            address = pyperclip.paste()
+
+        # Out of all control-flow statements webbrowser's open function is called which will
+        # open a browser with starting URL and append address variable to that URL
+        webbrowser.open('https://www.google.com/maps/place/' + address)
+
+        # Asks user if he wants to continue and stores the answer in again variable
+        again = input('Do you want to continue? (yes/no):')
+
+        if again == 'yes': # If answer is yes, variable args is assigned again but with different value
+            args = input('Enter another address and city: ')
+            continue # Also it comes back at the start of while loop and does all the work again.
+        else:
+            break # If anything else is typed as the answer, loop breaks.
+
+
+# Taking user's input for wishful location and storing it in sys.argv
 sys.argv = input('Enter address and city: ')
+# Calling openGMaps with sys.argv as the argument.
+openGMaps(sys.argv)
 
-if len(sys.argv) > 1:
-    address = ''.join(sys.argv[:])
-else:
-    address = pyperclip.paste()
-
-webbrowser.open('https://www.google.com/maps/place/' + address)
-
+# When loop breaks, godbye message is printed and program is over.
+print('Godbye!')
 
